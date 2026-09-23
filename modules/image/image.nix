1e4dd3c72e15b71cf3f484edd3cebbb6d2d8f0cf
MadMcCrow <noe.perard+git@gmail.com>
2026-09-23 14:01:46 +0200

    add image default settings

---
+++ modules/image/image.nix
@@ -0,0 +1,18 @@
+{
+  modConfig,
+  mkOptions,
+  inputs,
+  ...
+}:
+{
+  lib,
+  config,
+  ...
+}:
+with (modConfig config);
+{
+  # options = mkOptions { };
+  config = mkIfEnable {
+    system.image.id = "nonOS";
+    };
+}
